cf15fee4d46355a2c3fe8f86962339fb5b587593
Daniel Auerbach <dauerbach13@gmail.com>
2024-02-15 17:48:05 -0500

    password change form on froont end

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -8,7 +8,7 @@
           <h3>Account Info:</h3>
           <div v-if="editingName" class="row mt-3">
             <div class="col-3">Name:</div>
-            <div class="col-5">
+            <div class="col-6 col-md-5">
               <input
                 type="text"
                 class="form-control"
@@ -18,9 +18,9 @@
                 aria-describedby="basic-addon1"
               />
             </div>
-            <div class="col-3">
+            <div class="col-2 col-md-3">
               <button v-if="loadingName" type="button" class="btn btn-outline-warning btn-spinner">
-                <div class="spinner-border" role="status">
+                <div class="spinner-border spinner-border-sm" role="status">
                   <span class="visually-hidden">Loading...</span>
                 </div>
               </button>
@@ -29,16 +29,16 @@
           </div>
           <div v-else class="row mt-3">
             <div class="col-3">Name:</div>
-            <div class="col-5">
+            <div class="col-6 col-md-5">
               {{ user.name }}
             </div>
-            <div class="col-3">
+            <div class="col-2 col-md-3">
               <button class="btn btn-outline-info" @click="editingName = true">Change</button>
             </div>
           </div>
           <div v-if="editingEmail" class="row mt-3">
             <div class="col-3">Email:</div>
-            <div class="col-5">
+            <div class="col-6 col-md-5">
               <input
                 type="text"
                 class="form-control"
@@ -48,9 +48,9 @@
                 aria-describedby="basic-addon1"
               />
             </div>
-            <div class="col-3">
+            <div class="col-2 col-md-3">
               <button v-if="loadingEmail" type="button" class="btn btn-outline-warning btn-spinner">
-                <div class="spinner-border" role="status">
+                <div class="spinner-border spinner-border-sm" role="status">
                   <span class="visually-hidden">Loading...</span>
                 </div>
               </button>
@@ -59,16 +59,68 @@
           </div>
           <div v-else class="row mt-3">
             <div class="col-3">Email:</div>
-            <div class="col-5">
+            <div class="col-6 col-md-5">
               {{ user.email }}
             </div>
-            <div class="col-3">
+            <div class="col-2">
               <button class="btn btn-outline-info" @click="editingEmail = true">Change</button>
             </div>
           </div>
-          <div class="row mt-3">
+          <div v-if="editingPassword" class="row mt-3">
+            <div class="col-12 pt-2 col-md-3 pt-md-0">
+              <label for="currentPassword" class="form-label" :class="{ invalid: !currentPasswordValid }"
+                >Current Password</label
+              >
+              <PasswordInput
+                id="currentPassword"
+                @input-password="
+                  (password) => {
+                    currentPassword = password;
+                  }
+                "
+              />
+              <p v-if="!currentPasswordValid" class="invalid">Incorrect password.</p>
+            </div>
+            <div class="col-12 pt-2 col-md-3 pt-md-0">
+              <label for="newPassword" class="form-label" :class="{ invalid: !newPasswordValid }">New Password</label>
+              <PasswordInput
+                id="newPassword"
+                @input-password="
+                  (password) => {
+                    newPassword = password;
+                    validateNewPassword();
+                  }
+                "
+              />
+              <p v-if="!newPasswordValid" class="invalid">Password must be at least 8 characters long.</p>
+            </div>
+            <div class="col-12 pt-2 col-md-3 pt-md-0">
+              <label for="newPasswordConfirm" class="form-label" :class="{ invalid: !newPasswordConfirmValid }"
+                >Confirm New Password</label
+              >
+              <PasswordInput
+                id="newPasswordConfirm"
+                @input-password="
+                  (password) => {
+                    newPasswordConfirm = password;
+                    validateNewPasswordConfirm();
+                  }
+                "
+              />
+              <p v-if="!newPasswordConfirmValid" class="invalid">Passwords must match.</p>
+            </div>
+            <div class="col-12 col-md-3 pt-md-0 pt-2rem">
+              <button v-if="loadingPassword" type="button" class="btn btn-outline-warning btn-spinner-password">
+                <div class="spinner-border spinner-border-sm" role="status">
+                  <span class="visually-hidden">Loading...</span>
+                </div>
+              </button>
+              <button v-else type="button" class="btn btn-outline-info" @click="changePassword">Save</button>
+            </div>
+          </div>
+          <div v-else class="row mt-3">
             <div class="col-6">
-              <button class="btn btn-outline-warning" @click="changePassword">Change Password</button>
+              <button class="btn btn-outline-warning" @click="editingPassword = true">Change Password</button>
             </div>
           </div>
         </div>
@@ -95,10 +147,18 @@ export default {
     return {
       userName: null,
       userEmail: null,
+      currentPassword: "",
+      newPassword: "",
+      newPasswordConfirm: "",
       editingName: false,
       editingEmail: false,
+      editingPassword: false,
       loadingName: false,
       loadingEmail: false,
+      loadingPassword: false,
+      currentPasswordValid: true,
+      newPasswordValid: true,
+      newPasswordConfirmValid: true,
     };
   },
   computed: {
@@ -137,15 +197,53 @@ export default {
         });
     },
     changePassword() {
-      console.log("uhm");
+      const vm = this;
+      vm.loadingPassword = true;
+      setTimeout(() => {
+        vm.editingPassword = vm.loadingPassword = false;
+      }, 1000);
+    },
+    validateNewPassword() {
+      const vm = this;
+      // password validation
+      if (vm.newPassword.length < 8) {
+        vm.newPasswordValid = false;
+      } else {
+        vm.newPasswordValid = true;
+      }
+      vm.validateNewPasswordConfirm();
+    },
+    validateNewPasswordConfirm() {
+      const vm = this;
+      // password confirm validation
+      if (vm.newPassword != vm.newPasswordConfirm) {
+        vm.newPasswordConfirmValid = false;
+      } else {
+        vm.newPasswordConfirmValid = true;
+      }
     },
   },
 };
 </script>
 
 <style scoped lang="scss">
+@media only screen and (min-width: 768px) {
+  .pt-2rem {
+    padding-top: 2rem !important;
+  }
+}
+.pt-2rem {
+  padding-top: 1rem;
+}
 .btn-spinner {
   width: 82.04px;
   height: 38px;
+}
+.btn-spinner-password {
+  width: 59.81px;
+  height: 38px;
+}
+.invalid {
+  color: #d91313;
 }
 </style>
