57d6111344882e48fc41a8c306bbe52802a38fbf
Daniel Auerbach <dauerbach13@gmail.com>
2024-12-31 11:36:48 -0500

    capitalize SOTW

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -21,7 +21,7 @@
             <li v-if="isLoggedIn" class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarResultsDropdown" role="button"
                 data-bs-toggle="dropdown" aria-expanded="false">
-                Sotw
+                SOTW
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarResultsDropdown">
                 <span data-bs-dismiss="offcanvas">
