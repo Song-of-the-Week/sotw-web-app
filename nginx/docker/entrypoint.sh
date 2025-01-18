6c268f255fc5e470b8eeecfe200b659eede3955b
Reddig <crreddig@gmail.com>
2025-01-18 11:44:40 -0500

    Try to fix nginx image

--- nginx/docker/entrypoint.sh
+++ nginx/docker/entrypoint.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env sh
-set -eu
+#!/bin/bash
 
-envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
-
-exec "$@"
+envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+while [ ! -f /etc/letsencrypt/live/example.com/fullchain.pem ]; do
+    echo "Waiting for SSL certificates..."
+    sleep 5
+done
+echo "Certificates available. Starting NGINX..."
+exec nginx -g "daemon off;"
