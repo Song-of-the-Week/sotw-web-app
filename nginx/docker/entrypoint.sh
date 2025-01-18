67f59a91f0f5ea534abc5e11e764684ad7ff911e
Reddig <crreddig@gmail.com>
2025-01-18 12:36:29 -0500

    Use domain name

--- nginx/docker/entrypoint.sh
+++ nginx/docker/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
+if [ -z "$DOMAIN_NAME" ]; then
+  echo "Error: DOMAIN_NAME environment variable is not set."
+  exit 1
+fi
 envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
-while [ ! -f /etc/letsencrypt/live/example.com/fullchain.pem ]; do
-    echo "Waiting for SSL certificates..."
-    sleep 5
+echo "Checking for SSL certificates for $DOMAIN_NAME..."
+
+while [ ! -f "/etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem" ]; do
+  echo "Waiting for SSL certificates to be generated for $DOMAIN_NAME..."
+  sleep 5
 done
-echo "Certificates available. Starting NGINX..."
+
+echo "SSL certificates for $DOMAIN_NAME found. Starting NGINX..."
 exec nginx -g "daemon off;"
