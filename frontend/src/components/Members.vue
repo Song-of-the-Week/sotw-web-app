aedd49368c7cea13a22ad89bc166728750d9ab0e
Reddig <crreddig@gmail.com>
2025-02-02 13:31:57 -0500

    Update component to be prettier

--- frontend/src/components/Members.vue
+++ frontend/src/components/Members.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="container">
-    <h2>Members</h2>
     <div v-if="loading" class="spinner-border" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
-    <ul v-else class="list-group">
-      <li v-for="member in members" :key="member.id" class="list-group-item">
-        {{ member.name }}
-      </li>
-    </ul>
+    <div v-else class="d-flex justify-content-center">
+      <div class="card" style="width: 30rem;">
+        <div class="card-header">
+          Members
+        </div>
+        <div class="card-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Playlist</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="member in members" :key="member.id">
+                <td>{{ member.name }}</td>
+                <td>
+                  <a :href="member.playlistLink" target="_blank" class="btn btn-primary">
+                    <i class="bi bi-spotify me-2"></i>
+                    View Playlist
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,7 +49,10 @@ export default {
     try {
       const sotwId = this.$route.params.sotwId;
       const response = await api.methods.apiGetSotwMembers(sotwId);
-      this.members = response.data;
+      this.members = response.data.map(member => ({
+        ...member,
+        playlistLink: member.playlists.length > 0 ? member.playlists[0].playlist_link : '#'
+      }));
     } catch (error) {
       console.error('Error fetching members:', error);
     } finally {
